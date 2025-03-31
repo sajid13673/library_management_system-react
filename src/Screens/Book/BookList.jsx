dbba057c7427d926aec0845a757455babe3e8605
Sajid Ahamed <sajid13673@gmail.com>
2025-03-31 23:53:19 +0530

    refactor(bookList): Remove unnecessary commented code and improve structure - Cleaned up the codebase by removing unused and redundant commented-out code for better readability. - Refactored the bookList feature for enhanced maintainability and optimized logic.

--- src/Screens/Book/BookList.jsx
+++ src/Screens/Book/BookList.jsx
@@ -72,168 +72,88 @@ export default function BookList(props) {
     getBooks();
   }, [page, orderBy]);
   return (
-    // <Box p={3} flex={1} display="flex" flexDirection="column" gap={2}>
-    //   <Box
-    //       component="form"
-    //       onSubmit={handleSearch}
-    //       noValidate
-    //       sx={{
-    //         display: "flex",
-    //         flexDirection: "row",
-    //         width: "10rem",
-    //         ml: "auto",
-    //         mr: 3,
-    //       }}
-    //     >
-    //       <InputBase
-    //         sx={{ ml: 1, flex: 1 }}
-    //         placeholder="Search"
-    //         inputProps={{ "aria-label": "search google maps" }}
-    //         onChange={handleSearchChange}
-    //         value={searchTerm}
-    //       />
-    //       <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-    //         <SearchIcon />
-    //       </IconButton>
-    //     </Box>
-    //   <FormControl sx={{ ml: "auto", mr: 3 }}>
-    //     <InputLabel variant="standard" htmlFor="uncontrolled-native">
-    //       Sort by
-    //     </InputLabel>
-    //     <NativeSelect
-    //       inputProps={{
-    //         name: "filter",
-    //         id: "uncontrolled-native",
-    //       }}
-    //       onChange={handleOrderByChange}
-    //     >
-    //       <option value={"created_at-desc"}>New to old</option>
-    //       <option value={"created_at-asc"}>Old to new</option>
-    //       <option value={"title-asc"}>Title ascending</option>
-    //       <option value={"title-desc"}>Title descending</option>
-    //       <option value={"year-desc"}>Year descending</option>
-    //       <option value={"year-asc"}>Year ascending</option>
-    //     </NativeSelect>
-    //   </FormControl>
-    //   <Grid container spacing={2}>
-    //     {data.length > 0 ? (
-    //       data.map((row) => (
-    //         <BookCard
-    //           key={row.id}
-    //           id={row.id}
-    //           title={row.title}
-    //           author={row.author}
-    //           publisher={row.publisher}
-    //           year={row.year}
-    //           path={row.path}
-    //           status={row.status}
-    //           activeBorrowings={row.activeBorrowings}
-    //           defaultImage={props.defaultImage}
-    //           handleBookEdit={(id) => handleBookEdit(id)}
-    //           handleBookDelete={(id) => handleBookDelete(id)}
-    //           handleAddBrowing={(bookId) => handleAddBrowing(bookId)}
-    //         />
-    //       ))
-    //     ) : (
-    //       <Container className="keyMessage">
-    //         {!loading ? (
-    //           <Typography variant="h3">Nothing to show</Typography>
-    //         ) : (
-    //           <Loading />
-    //         )}
-    //       </Container>
-    //     )}
-    //   </Grid>
-    //   <Stack spacing={2} sx={{ mt: "auto" }}>
-    //     <Pagination
-    //       count={totalPages}
-    //       page={page}
-    //       color="primary"
-    //       onChange={handleChange}
-    //     />
-    //   </Stack>
-    // </Box>
-
     <Box p={3} flex={1} display="flex" flexDirection="column" gap={1}>
-      {!loading && <Box
-        component="form"
-        onSubmit={handleSearch}
-        noValidate
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          width: "10rem",
-          ml: "auto",
-          mr: 3,
-        }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search"
-          inputProps={{ "aria-label": "search google maps" }}
-          onChange={handleSearchChange}
-          value={searchTerm}
-        />
-        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-      </Box>}
+      {!loading && (
+        <Box
+          component="form"
+          onSubmit={handleSearch}
+          noValidate
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            width: "10rem",
+            ml: "auto",
+            mr: 3,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search"
+            inputProps={{ "aria-label": "search google maps" }}
+            onChange={handleSearchChange}
+            value={searchTerm}
+          />
+          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Box>
+      )}
       {!loading ? (
         <>
-        {data.length > 0 ? (
-          <Box display="flex" flexDirection="column" gap={2}>
-          <FormControl sx={{ ml: "auto", mr: 3 }}>
-            <InputLabel variant="standard" htmlFor="uncontrolled-native">
-              Sort by
-            </InputLabel>
-            <NativeSelect
-              inputProps={{
-                name: "filter",
-                id: "uncontrolled-native",
-              }}
-              onChange={handleOrderByChange}
-            >
-              <option value={"created_at-desc"}>New to old</option>
-              <option value={"created_at-asc"}>Old to new</option>
-              <option value={"title-asc"}>Title ascending</option>
-              <option value={"title-desc"}>Title descending</option>
-              <option value={"year-desc"}>Year descending</option>
-              <option value={"year-asc"}>Year ascending</option>
-            </NativeSelect>
-          </FormControl>
-          <Grid container spacing={2}>
-            {data.map((row) => (
-              <BookCard
-                key={row.id}
-                id={row.id}
-                title={row.title}
-                author={row.author}
-                publisher={row.publisher}
-                year={row.year}
-                path={row.path}
-                status={row.status}
-                activeBorrowings={row.activeBorrowings}
-                defaultImage={props.defaultImage}
-                handleBookEdit={(id) => handleBookEdit(id)}
-                handleBookDelete={(id) => handleBookDelete(id)}
-                handleAddBrowing={(bookId) => handleAddBrowing(bookId)}
-              />
-            ))}
-          </Grid>
-          <Stack spacing={2} sx={{ mt: "auto" }}>
-            <Pagination
-              count={totalPages}
-              page={page}
-              color="primary"
-              onChange={handleChange}
-            />
-          </Stack>
-        </Box>) : 
-        (
-          <Container className="keyMessage">
-            <Typography variant="h3">Nothing to show</Typography>
-          </Container>
-         )}
+          {data.length > 0 ? (
+            <Box display="flex" flexDirection="column" gap={2}>
+              <FormControl sx={{ ml: "auto", mr: 3 }}>
+                <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                  Sort by
+                </InputLabel>
+                <NativeSelect
+                  inputProps={{
+                    name: "filter",
+                    id: "uncontrolled-native",
+                  }}
+                  onChange={handleOrderByChange}
+                >
+                  <option value={"created_at-desc"}>New to old</option>
+                  <option value={"created_at-asc"}>Old to new</option>
+                  <option value={"title-asc"}>Title ascending</option>
+                  <option value={"title-desc"}>Title descending</option>
+                  <option value={"year-desc"}>Year descending</option>
+                  <option value={"year-asc"}>Year ascending</option>
+                </NativeSelect>
+              </FormControl>
+              <Grid container spacing={2}>
+                {data.map((row) => (
+                  <BookCard
+                    key={row.id}
+                    id={row.id}
+                    title={row.title}
+                    author={row.author}
+                    publisher={row.publisher}
+                    year={row.year}
+                    path={row.path}
+                    status={row.status}
+                    activeBorrowings={row.activeBorrowings}
+                    defaultImage={props.defaultImage}
+                    handleBookEdit={(id) => handleBookEdit(id)}
+                    handleBookDelete={(id) => handleBookDelete(id)}
+                    handleAddBrowing={(bookId) => handleAddBrowing(bookId)}
+                  />
+                ))}
+              </Grid>
+              <Stack spacing={2} sx={{ mt: "auto" }}>
+                <Pagination
+                  count={totalPages}
+                  page={page}
+                  color="primary"
+                  onChange={handleChange}
+                />
+              </Stack>
+            </Box>
+          ) : (
+            <Container className="keyMessage">
+              <Typography variant="h3">Nothing to show</Typography>
+            </Container>
+          )}
         </>
       ) : (
         <Loading />
